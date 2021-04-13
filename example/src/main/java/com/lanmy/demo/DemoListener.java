31e6cae457b9d9c134928cefbbd9c0075f797a4b
lanmingyong <8517626622@qq.com>
2021-04-13 21:54:20 +0800

    第一次提交

---
+++ example/src/main/java/com/lanmy/demo/DemoListener.java
@@ -0,0 +1,185 @@
+package com.lanmy.demo;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRequestAttributeEvent;
+import javax.servlet.ServletRequestAttributeListener;
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionActivationListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionBindingListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class DemoListener
+ *
+ */
+@WebListener
+public class DemoListener implements ServletContextListener, ServletContextAttributeListener, HttpSessionListener, HttpSessionAttributeListener, HttpSessionActivationListener, HttpSessionBindingListener, ServletRequestListener, ServletRequestAttributeListener {
+
+    /**
+     * Default constructor. 
+     */
+    public DemoListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("sessionCreated...");
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
+     */
+    public void attributeRemoved(ServletContextAttributeEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("attributeRemoved...");
+    }
+
+	/**
+     * @see ServletRequestAttributeListener#attributeAdded(ServletRequestAttributeEvent)
+     */
+    public void attributeAdded(ServletRequestAttributeEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("attributeAdded...");
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeReplaced(HttpSessionBindingEvent)
+     */
+    public void attributeReplaced(HttpSessionBindingEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("attributeReplaced...");
+    }
+
+	/**
+     * @see HttpSessionActivationListener#sessionWillPassivate(HttpSessionEvent)
+     */
+    public void sessionWillPassivate(HttpSessionEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("sessionWillPassivate...");
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("contextInitialized...");
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
+     */
+    public void attributeAdded(ServletContextAttributeEvent event)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("attributeAdded...");
+    }
+
+	/**
+     * @see ServletRequestListener#requestDestroyed(ServletRequestEvent)
+     */
+    public void requestDestroyed(ServletRequestEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("requestDestroyed...");
+    }
+
+	/**
+     * @see ServletRequestAttributeListener#attributeRemoved(ServletRequestAttributeEvent)
+     */
+    public void attributeRemoved(ServletRequestAttributeEvent srae)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("attributeRemoved...");
+    }
+
+	/**
+     * @see HttpSessionBindingListener#valueBound(HttpSessionBindingEvent)
+     */
+    public void valueBound(HttpSessionBindingEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("valueBound...");
+    }
+
+	/**
+     * @see ServletRequestListener#requestInitialized(ServletRequestEvent)
+     */
+    public void requestInitialized(ServletRequestEvent sre)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("requestInitialized...");
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent se)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("sessionDestroyed...");
+    }
+
+	/**
+     * @see HttpSessionActivationListener#sessionDidActivate(HttpSessionEvent)
+     */
+    public void sessionDidActivate(HttpSessionEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("sessionDidActivate...");
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("contextDestroyed...");
+    }
+
+	/**
+     * @see ServletRequestAttributeListener#attributeReplaced(ServletRequestAttributeEvent)
+     */
+    public void attributeReplaced(ServletRequestAttributeEvent srae)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("attributeReplaced...");
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
+     */
+    public void attributeAdded(HttpSessionBindingEvent event)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("attributeAdded...");
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeRemoved(HttpSessionBindingEvent)
+     */
+    public void attributeRemoved(HttpSessionBindingEvent event)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("attributeRemoved...");
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
+     */
+    public void attributeReplaced(ServletContextAttributeEvent event)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("attributeReplaced...");
+    }
+
+	/**
+     * @see HttpSessionBindingListener#valueUnbound(HttpSessionBindingEvent)
+     */
+    public void valueUnbound(HttpSessionBindingEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("valueUnbound...");
+    }
+	
+}
